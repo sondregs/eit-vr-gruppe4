08334915b6fcb6a9974f096e2f2e6509bb315411
andreashb2 <andreas.nor@hotmail.com>
2020-03-11 14:58:46 +0100

    Remove unnecessary files and clean up some code

--- run.py
+++ run.py
@@ -1,28 +1,30 @@
 from os import walk
+import os
 from time import sleep
-
 import serial
-
 from record import take_pic
-from Gps.Neo6mGPS import getGPS
+from gps import get_gps
 
 
 def list_files(directory, extension):
     file_list = []
     for (dirpath, dirnames, filenames) in walk(directory):
-        file_list += [f for f in filenames if f.endswith('.' + extension)]
+        file_list += [os.path.join(dirpath,f) for f in filenames if f.endswith('.' + extension)]
     return file_list
 
-num = 3
-path = '/media/pi/'
-serialPort = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
-old_files = list_files(path, 'jpg')
-for i in range(num):
-    take_pic() # Takes picture
-    gps = getGPS()
-    print(gps)
-    sleep(4) # 4 is the lowest working sleep time
-    new_files = list_files(path, 'jpg')
-    new_file = list(set(new_files) - set(old_files))[0] # Selects first file if somehow multiple new files
-    print(new_file)
-    old_files = new_files
+if __name__ == '__main__':
+    num = 3
+    path = '/media/pi/'
+    serialPort = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
+    old_files = list_files(path, 'jpg')
+    for i in range(num):
+        take_pic() # Takes picture
+        gps = get_gps()
+        print(gps)
+        sleep(4) # 4 is the lowest working sleep time
+        new_files = list_files(path, 'jpg')
+        new_file = list(set(new_files) - set(old_files))[0] # Selects first file if somehow multiple new files
+        old_files = new_files
+        print(new_file)
+        with open('log.txt', 'a') as file:
+            file.write('{"file": "%s", "gps": "%s""}\n' % (new_file, gps))
