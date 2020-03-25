fb3b0490d84855f08f6091ada47223aa2d3669f6
Dabble <ddabble@users.noreply.github.com>
2020-03-25 12:05:13 +0100

    Fixed running win32 code on other platforms

--- message/__init__.py
+++ message/__init__.py
@@ -1,4 +1,8 @@
-from multiprocess import popen_spawn_win32
+import sys
 
-# Fixes "No module named 'multiprocess'" error when calling functions with `wrapt_timeout_decorator.timeout`
-popen_spawn_win32.WINENV = False
+
+if sys.platform.startswith('win32'):
+    from multiprocess import popen_spawn_win32
+
+    # Fixes "No module named 'multiprocess'" error when calling functions with `wrapt_timeout_decorator.timeout`
+    popen_spawn_win32.WINENV = False
